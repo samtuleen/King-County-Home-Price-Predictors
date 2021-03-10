52bf3a401fd2accef751f2af9a71dad9b38aaf7d
Sameeha Ramadhan <shramadhan@gmail.com>
2021-03-10 09:52:27 -0600

    Changes

---
+++ src/modeling.py
@@ -0,0 +1,159 @@
+# imports:
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+from statsmodels.formula.api import ols
+import statsmodels.api as sm
+import scipy.stats as stats
+
+
+def check_for_assumptions(modelname):
+#     rsquared = modelname.rsquared
+#     params = modelname.params
+#     print(f'Rsquared of Model: {rsquared}')
+#     print('----------')
+#     print('Beta values of Model:')
+#     print(params)
+#     rainbow_statistic, rainbow_p_value = linear_rainbow(model1)
+#     print("Rainbow statistic:", rainbow_statistic)
+#     print("Rainbow p-value:", rainbow_p_value)
+    fig, ax = plt.subplots(1,2, figsize=(12,6))
+    residuals = modelname.resid
+    fig = sm.graphics.qqplot(residuals, dist=stats.norm, line='45', fit=True, ax=ax[0])
+    ax[0].set_title('QQ-Plot of Residuals')
+    ax[1].scatter(modelname.predict(), modelname.resid)
+    sns.set(font_scale = 1)
+    ax[1].set_title('Homoscadasicity Assumption')
+    plt.xlabel('Model Predictions')
+    plt.ylabel('Model Residuals')
+    ax[1].plot(modelname.predict(), [0 for i in range(len(df))], color = 'maroon')
+    fig.tight_layout()
+    return plt.show()
+
+
+def z_score(feature, df):
+    """
+    feaure is a string of the feature whose z-score to obtain 
+    df is the dataframe in which the feature can be found
+    """
+    z_name = 'z_' + feature
+    df[z_name] = (df[feature] - df[feature].mean()) / df[feature].std()
+    
+    return df
+
+
+def model(features_lst, df, target_var):
+    """
+    features_lst is a list of strings of the features to use from the df
+    df is the data frame to subset the features from 
+    target_var is a string of the target variable
+    """
+
+    # Create a dataframe with only the target and the chosen corellation feature
+    df_model = df[features_lst]
+
+    # build the R-style formula.
+    target = target_var
+    features_lstcopy = features_lst.copy()
+    features_lstcopy.remove(target)
+    x_vals = features_lstcopy
+    x_formula = '+'.join(x_vals)
+    formula = target + '~' + x_formula
+
+    # Fit the model on the dataframe composed of the two features
+    model = ols(formula=formula, data=df_model).fit()
+    
+    print(model.summary())
+    # view r^2 and model summary:
+    
+    # check assumptions
+#     normality_assumption(model)
+#     homo_assumption(model, df)
+    
+    return model
+
+
+def normality_assumption(model):
+    # plot normality assumption
+    fig, ax = plt.subplots(figsize = (15, 10))
+    figure = sm.graphics.qqplot(model.resid, dist=stats.norm, line='45', fit=True, ax = ax);
+    ax.set_title('QQ-Plot of Residuals', fontsize = 25)
+    return plt.show()
+
+
+def homo_assumption(model, df):
+    # plot homoscadasicity assumption
+    fig, ax = plt.subplots(figsize = (15, 10))
+    plt.scatter(model.predict(), model.resid)
+    sns.set(font_scale = 1)
+    fig.suptitle('Scatter Plot of Model Predictions vs. Residuals', fontsize = 25)
+    # ax.set_title('Homoscadasicity Assumption Not Met', fontsize = 20) - change this to an iff statement maybe...
+    plt.xlabel('Model Predictions', fontsize = 18)
+    plt.ylabel('Model Residuals', fontsize = 18)
+    ax.tick_params(labelsize=10)
+    plt.plot(model.predict(), [0 for i in range(len(df))], color = 'red')
+    return plt.show()
+
+def heatmap_multi(x_features, df):
+    """
+    Creates a heatmap of all the x features in a model to show multicollinearity.
+    x_features (lst):  A list of strings of the column header names of the x features in the model
+    df:  the dataframe where the features belong
+    Returns the plotted heatmap
+    """
+    df_x_feats = df.loc[:, x_features]
+
+    x_corrs = df_x_feats.corr()
+
+    mask = np.triu(np.ones_like(x_corrs, dtype=np.bool))
+    f, ax = plt.subplots(figsize = (18, 16))
+    sns.heatmap(x_corrs, mask = mask, vmax = 0.3, 
+            center = 0, square = True, linewidths = 0.5, 
+            cbar_kws = {'shrink': 0.5})
+    ax.tick_params(axis='both', which='major', labelsize=20, labelrotation = 45)
+    ax.set_title('Feature Multicollinearity Heat Map', fontsize = 30)
+    
+    return plt.show()
+
+
+def cohen_d(group1, group2):
+    """
+    Takes two groups and returns the cohen d effect size
+    """
+    diff = group1.mean() - group2.mean()
+
+    n1, n2 = len(group1), len(group2)
+    var1 = group1.var()
+    var2 = group2.var()
+    pooled_var = (n1 * var1 + n2 * var2) / (n1 + n2)
+    
+    d = diff / np.sqrt(pooled_var)
+    print(f"Cohens d Effect Size for Difference in Home Prices of The 2 Groups: {d}")
+    
+    return d
+
+
+def ttest_vis(group1, group2, test_result):
+    """
+    Takes 2 samples and a t-test result and returns a visualization of the critical value and t statistic.
+    """
+    xs = np.linspace(-4, 4, 200)
+    # use stats.t.pdf to get values on the probability density function for the t-distribution
+    # the second argument is the degrees of freedom
+    ys = stats.t.pdf(xs, len(group1)+len(group2)-2, 0, 1)
+    t_crit = np.round(stats.t.ppf(1 - 0.05, df=len(group1)+len(group2)-2),3)
+
+    fig = plt.figure(figsize=(15,8))
+    ax = fig.gca()
+
+    # plot the lines using matplotlib's plot function:
+    ax.plot(xs, ys, linewidth=3, color='maroon', alpha=.75)
+
+    ax.axvline(t_crit,color='green',linestyle='--',lw=4,label='critical t-value', alpha=.75)
+    ax.axvline(test_result[0],color='red',linestyle='--',lw=4,label='test t-stat', alpha=.75)
+    ax.legend()
+    ax.fill_betweenx(ys,xs,t_crit,where= xs > t_crit)
+    return plt.show()
